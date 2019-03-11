164332529fe8e17654f758301759496d4b4217e8
hybridbad <hybridbad90@gmail.com>
2019-03-11 17:34:12 +0000

    Further clean up of TDDcode, dispose of vacuous tests and uncomplicate code

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,15 +1,16 @@
 class Oystercard
-  START_BALANCE = 0
+
   MAX_BALANCE = 90
+
   attr_reader :balance
 
-  def initialize(balance = START_BALANCE, journey = false)
-    @balance = balance
+  def initialize(journey = false)
+    @balance = 0
     @journey = journey
   end
 
   def top_up(amount)
-    fail 'Max balance reached' if (amount + balance) > MAX_BALANCE
+    fail 'Maximum balance of 90 exceeded' if amount + balance > MAX_BALANCE
     @balance += amount
   end
 
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -3,42 +3,42 @@ require 'oystercard'
 describe Oystercard do
 
   subject(:card) {Oystercard.new}
+
   describe '#Balance' do
 
     it 'default balance is zero' do
-      expect(card.balance).to eq Oystercard::START_BALANCE
+      expect(card.balance).to eq 0
     end
+
   end
 
   describe '#Top_up' do
-    it 'Responds to top_up method' do
-      expect(card).to respond_to(:top_up)
-    end
 
     it 'Balance should change when topped up' do
-      expect(card.top_up(5)).to eq(5)
-      #expect {top_up}.to change {balance}.by(5)
+      expect{ card.top_up 30}.to change{ subject.balance }.by 30
     end
 
-    it 'Error msg raised when max balance reached' do
-      expect{ card.top_up(100) }.to raise_error('Max balance reached')
+    it 'raises an error if the maximum balance is exceeded' do
+      maximum_balance = Oystercard::MAX_BALANCE
+      card.top_up(maximum_balance)
+      expect{ card.top_up 1 }.to raise_error("Maximum balance of #{maximum_balance} exceeded")
     end
+
   end
 
-  
-
   describe '#deduct' do 
-    it 'responds to deduct method' do 
-      expect(card).to respond_to(:deduct)
-    end 
+
+    it { is_expected.to respond_to(:deduct).with(1).argument }
 
     it 'balance should change when deducted' do 
-      expect(card.deduct(2)).to eq(-2)
+      card.top_up(20)
+      expect{ card.deduct 3}.to change{ card.balance }.by -3
     end
+
   end
 
   describe '#journey' do 
-    
+
     it 'allows the customer to touch in and pass through the barriers' do
       expect(card).to respond_to(:touch_in)
     end
@@ -54,6 +54,7 @@ describe Oystercard do
     it 'ends a journey when you touch out' do 
       expect(card.touch_out).to eq(false)
     end
+
   end
 
 end
