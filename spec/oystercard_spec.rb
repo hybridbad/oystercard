c85c13a07aa13214babefe715330520408d11be0
hybridbad <hybridbad90@gmail.com>
2019-03-11 17:12:38 +0000

    Linted and fixed readme

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -19,41 +19,42 @@ describe Oystercard do
       expect(card.top_up(5)).to eq(5)
       #expect {top_up}.to change {balance}.by(5)
     end
+
+    it 'Error msg raised when max balance reached' do
+      expect{ card.top_up(100) }.to raise_error('Max balance reached')
+    end
   end
 
-  it 'Error msg raised when max balance reached' do
-    expect{ card.top_up(100) }.to raise_error('Max balance reached')
-  end
+  
 
   describe '#deduct' do 
     it 'responds to deduct method' do 
       expect(card).to respond_to(:deduct)
     end 
 
-  it 'balance should change when deducted' do 
-    expect(card.deduct(2)).to eq(-2)
+    it 'balance should change when deducted' do 
+      expect(card.deduct(2)).to eq(-2)
+    end
   end
-end
 
-   describe '#journey' do 
+  describe '#journey' do 
+    
     it 'allows the customer to touch in and pass through the barriers' do
-    expect(card).to respond_to(:touch_in)
+      expect(card).to respond_to(:touch_in)
+    end
+
+    it 'allows the customer to touch out and pass through the barriers' do
+      expect(card).to respond_to(:touch_out)
+    end
+
+    it 'starts a journey when you touch in' do 
+      expect(card.touch_in).to eq(true)
+    end
+
+    it 'ends a journey when you touch out' do 
+      expect(card.touch_out).to eq(false)
+    end
   end
-
-  it 'allows the customer to touch out and pass through the barriers' do
-    expect(card).to respond_to(:touch_out)
-  end
-
-   it 'starts a journey when you touch in' do 
-    expect(card.touch_in).to eq(true)
-  end
-
-   it 'ends a journey when you touch out' do 
-    expect(card.touch_out).to eq(false)
-  end
-
-
-end
 
 end
 
