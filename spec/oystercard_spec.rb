3e964b270ed534d5eb3cca0693cb0d9af45fb013
Harry Galliano <harrygalliano97@gmail.com>
2019-03-11 16:25:14 +0000

    added a deduct method for spending money

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -17,11 +17,23 @@ describe Oystercard do
 
     it 'Balance should change when topped up' do
       expect(card.top_up(5)).to eq(5)
+      #expect {top_up}.to change {balance}.by(5)
     end
   end
 
   it 'Error msg raised when max balance reached' do
     expect{ card.top_up(100) }.to raise_error('Max balance reached')
+  end
+
+  describe '#deduct' do 
+    it 'responds to deduct method' do 
+      expect(card).to respond_to(:deduct)
+    end 
+
+it 'balance should change when deducted' do 
+  expect(card.deduct(2)).to eq(-2)
+end
+
   end
 
 end
